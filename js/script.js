ed1af7ea36c5ad8bdb146567498ab744ff28ce6f
sviatlana <sviatlana.skiba@gmail.com>
2022-11-24 14:36:27 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
+import tabs from './modules/tabs';
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import { openModal } from './modules/modal';
+
 window.addEventListener('DOMContentLoaded', function() {
-    const tabs = require('./modules/tabs'),
-        calc = require('./modules/calc'),
-        cards = require('./modules/cards'),
-        forms = require('./modules/forms'),
-        modal = require('./modules/modal'),
-        slider = require('./modules/slider'),
-        timer = require('./modules/timer');
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
 
-
-    tabs();
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     calc();
     cards();
-    forms();
-    modal();
-    slider();
-    timer();
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        slide: '.offer__slide',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    timer('.timer', '2023-06-11');
 });
